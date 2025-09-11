8f5d5979b20571777df5b9914d10ee334aad4c5a
Tuan_c <55803554+TuanPointC@users.noreply.github.com>
2025-09-11 22:29:15 +0700

    remove attachment

--- back-end/odoo/addons/hrm/controllers/__init__.py
+++ back-end/odoo/addons/hrm/controllers/__init__.py
@@ -1,4 +1,3 @@
 from . import employee_api
 from . import auth_api
 from . import contract_api
-from . import attachment_api
--- back-end/odoo/addons/hrm/models/__init__.py
+++ back-end/odoo/addons/hrm/models/__init__.py
@@ -1,6 +1,5 @@
 from . import employee
 from . import contract
-from . import attachment
 from . import department
 from . import job
 from . import dept_position
