b75e894605a2d4b0c6b1f07bae36d6acee2ae999
Internship-Phung Quang Manh <phung.quang.manh@vsi-international.com>
2025-09-18 21:11:48 +0700

    Fix

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -46,78 +46,78 @@ export default function App() {
           <Route path={ROUTES_APP.home} element={<Home />} />
           <Route path={ROUTES_APP.notAuthenticated} element={<NotAuthenticated />} />
 
-          <Route element={<ProtectedRoute />}>
-            <Route element={<MainLayout />}>
-              <Route path={ROUTES_APP.profile} element={<Profile />} />
+          {/* <Route element={<ProtectedRoute />}> */}
+          <Route element={<MainLayout />}>
+            <Route path={ROUTES_APP.profile} element={<Profile />} />
 
-              {/* home crm */}
-              <Route path={ROUTES_APP.crm.homeCRM} element={<HomeCRM />} />
+            {/* home crm */}
+            <Route path={ROUTES_APP.crm.homeCRM} element={<HomeCRM />} />
 
-              {/* khách hàng */}
-              <Route path={ROUTES_APP.crm.customerList} element={<CustomerList />} />
-              <Route path={ROUTES_APP.crm.customerDetail} element={<CustomerDetail />} />
+            {/* khách hàng */}
+            <Route path={ROUTES_APP.crm.customerList} element={<CustomerList />} />
+            <Route path={ROUTES_APP.crm.customerDetail} element={<CustomerDetail />} />
 
-              {/* sản phẩm */}
-              <Route path={ROUTES_APP.crm.productPage} element={<ProductPage />} />
+            {/* sản phẩm */}
+            <Route path={ROUTES_APP.crm.productPage} element={<ProductPage />} />
 
-              {/* liên hệ */}
-              <Route path={ROUTES_APP.crm.contactList} element={<ContactList />} />
-              <Route path={ROUTES_APP.crm.contactDetail} element={<ContactDetail />} />
+            {/* liên hệ */}
+            <Route path={ROUTES_APP.crm.contactList} element={<ContactList />} />
+            <Route path={ROUTES_APP.crm.contactDetail} element={<ContactDetail />} />
 
-              {/* Mẫu báo giá  */}
-              <Route path={ROUTES_APP.crm.quotationList} element={<QuotationList />} />
-              <Route path={ROUTES_APP.crm.quotationDetail} element={<QuotationDetail />} />
+            {/* Mẫu báo giá  */}
+            <Route path={ROUTES_APP.crm.quotationList} element={<QuotationList />} />
+            <Route path={ROUTES_APP.crm.quotationDetail} element={<QuotationDetail />} />
 
-              {/* Cơ hội */}
-              <Route path={ROUTES_APP.crm.opportunityList} element={<OpportunityList />} />
-              <Route path={ROUTES_APP.crm.opportunityDetail} element={<OpportunityDetail />} />
+            {/* Cơ hội */}
+            <Route path={ROUTES_APP.crm.opportunityList} element={<OpportunityList />} />
+            <Route path={ROUTES_APP.crm.opportunityDetail} element={<OpportunityDetail />} />
 
-              {/* Khách tiềm năng  */}
-              <Route path={ROUTES_APP.crm.leadList} element={<LeadList />} />
-              <Route path={ROUTES_APP.crm.leadDetail} element={<LeadDetail />} />
+            {/* Khách tiềm năng  */}
+            <Route path={ROUTES_APP.crm.leadList} element={<LeadList />} />
+            <Route path={ROUTES_APP.crm.leadDetail} element={<LeadDetail />} />
 
-              {/* Hợp đồng  */}
-              <Route path={ROUTES_APP.crm.contractList} element={<ContractList />} />
-              <Route path={ROUTES_APP.crm.contractDetail} element={<ContractDetailWrapper />} />
+            {/* Hợp đồng  */}
+            <Route path={ROUTES_APP.crm.contractList} element={<ContractList />} />
+            <Route path={ROUTES_APP.crm.contractDetail} element={<ContractDetailWrapper />} />
 
-              {/* báo giá  */}
-              <Route path={ROUTES_APP.crm.quoteList} element={<QuoteList />} />
-              <Route path={ROUTES_APP.crm.quoteDetail} element={<QuoteDetailWrapper />} />
+            {/* báo giá  */}
+            <Route path={ROUTES_APP.crm.quoteList} element={<QuoteList />} />
+            <Route path={ROUTES_APP.crm.quoteDetail} element={<QuoteDetailWrapper />} />
 
-              {/* Báo cáo công nợ */}
-              <Route path={ROUTES_APP.crm.debtReportList} element={<DebtReportList />} />
-              <Route path={ROUTES_APP.crm.debtReportDetail} element={<DebtReportDetail />} />
+            {/* Báo cáo công nợ */}
+            <Route path={ROUTES_APP.crm.debtReportList} element={<DebtReportList />} />
+            <Route path={ROUTES_APP.crm.debtReportDetail} element={<DebtReportDetail />} />
 
-              {/* ++++++++++++++++++++++++++++ HRM +++++++++++++++++++++++++++++++++++++++++++ */}
-              {/* home hrm */}
-              <Route path={ROUTES_APP.hrm.homeHRM} element={<HomeHRM />} />
+            {/* ++++++++++++++++++++++++++++ HRM +++++++++++++++++++++++++++++++++++++++++++ */}
+            {/* home hrm */}
+            <Route path={ROUTES_APP.hrm.homeHRM} element={<HomeHRM />} />
 
-              {/* Nhân sự */}
-              <Route path={ROUTES_APP.hrm.employeeDetail} element={<EmployeeDetail />} />
-              <Route path={ROUTES_APP.hrm.employeeList} element={<EmployeeList />} />
+            {/* Nhân sự */}
+            <Route path={ROUTES_APP.hrm.employeeDetail} element={<EmployeeDetail />} />
+            <Route path={ROUTES_APP.hrm.employeeList} element={<EmployeeList />} />
 
-              {/* Chức vụ */}
-              <Route path={ROUTES_APP.hrm.positionList} element={<PositionList />} />
+            {/* Chức vụ */}
+            <Route path={ROUTES_APP.hrm.positionList} element={<PositionList />} />
 
-              {/* Phòng ban */}
-              <Route path={ROUTES_APP.hrm.departmentList} element={<DepartmentList />} />
+            {/* Phòng ban */}
+            <Route path={ROUTES_APP.hrm.departmentList} element={<DepartmentList />} />
 
-              {/* Tài sản */}
-              <Route path={ROUTES_APP.hrm.assetList} element={<AssetList />} />
-              <Route path={ROUTES_APP.hrm.assetDetail} element={<AssetDetail />} />
+            {/* Tài sản */}
+            <Route path={ROUTES_APP.hrm.assetList} element={<AssetList />} />
+            <Route path={ROUTES_APP.hrm.assetDetail} element={<AssetDetail />} />
 
-              {/* Mẫu đề xuất */}
-              <Route path={ROUTES_APP.hrm.proposalTemplateList} element={<ProposalTemplateList />} />
-              <Route
-                path={ROUTES_APP.hrm.proposalTemplateDetail}
-                element={<ProposalTemplateDetail />}
-              />
-            </Route>
+            {/* Mẫu đề xuất */}
+            <Route path={ROUTES_APP.hrm.proposalTemplateList} element={<ProposalTemplateList />} />
+            <Route
+              path={ROUTES_APP.hrm.proposalTemplateDetail}
+              element={<ProposalTemplateDetail />}
+            />
           </Route>
+          {/* </Route> */}
 
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
-    </ConfigProvider>
+    </ConfigProvider >
   );
 }
